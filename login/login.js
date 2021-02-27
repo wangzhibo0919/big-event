de3a24bdc94b078b2066c39f2cc6aec464ac8cb9
wzb0919 <17604624136>
2021-02-27 10:31:20 +0800

    login:去登陆功能完成

--- login/login.js
+++ login/login.js
@@ -1,3 +1,6 @@
+//引入layui.layer
+let layer = layui.layer;
+
 //切换功能
 //去注册
 $('#goto-register').on('click',function(){
@@ -8,4 +11,33 @@ $('#goto-register').on('click',function(){
 $('#goto-login').on('click',function(){
     $('#login').show()
     $('#register').hide()
+})
+
+
+// -------------------------------------------注册
+// html：form表单！按钮/表单元素 name属性和参数名一致！
+// JS ：收集数据！$.ajax()
+$('#register form').on('submit',function(e){
+  e.preventDefault();//阻止冒泡 
+  // 1.收集数据  JQ  layui.form模块
+  //收集数据
+  let data = $(this).serialize();//this指向的是form
+
+  //提交数据
+  $.ajax({
+      type:'post',
+      url:"http://ajax.frontend.itheima.net/api/reguser",
+      data:data,
+      success:function(res){
+          layer.msg(res.message)//layer.msg弹窗提醒
+          //判断是否注册成功
+          if (res.status == 0){
+              //去登陆
+              $('#goto-login').click();
+              //注册表单重置
+              //jq对象[0].reset()
+              $('#register form')[0].reset();
+          }
+      }
+  })
 })
